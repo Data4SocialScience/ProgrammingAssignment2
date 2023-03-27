f212fc48fdadb476b08619964dba7332f874f7f3
unknown <razazaidi.zaidi@gmail.com>
2023-03-27 19:45:20 +0500

    this is part of the assignment at coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function calculates the inverse matrix of an object and then cache (stores)
+## its value to recall for future use to avoid expensive calculations
+## again and again 
 
-## Write a short comment describing this function
+## The function makeCacheMatrix sets the `getters` and `setters` for next
+## calculation. It sets value of argument x and recalls x. And then it writes 
+## function to set, store and recall value of im(invese matrix).
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  im <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  getit <- function() x
+  
+  setInverseMatrix <- function(inv_mat) im <<- inv_mat
+  
+  getInverseMatrix <- function() im
+  
+  list(set = set, getit = getit, setInvMat = setInverseMatrix, getInvMat = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
+## Following function makes use of previous function. It first looks into memory
+## to look for value of `im`. If `im` is not null, then it retrieves the 
+## already saved `im` from x$getInvMat() function. But if `im`is NULL, then the function
+## attempts to calculate the inverse matrix and caches its values for further use
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  im <- x$getInvMat()
+  if(!is.null(im)) {
+    message("caching inverse matrix")
+    return(im)
+  }
+  
+  data <- x$getit()
+  im <- solve(data, ...)
+  x$setInvMat(im)
+  im
 }
